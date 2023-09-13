4a2d8288490bcd14779f1ec62183d0b53be8a9c2
Nathalia Oliveira <nathaliaolisil@gmail.com>
2023-09-13 20:53:08 -0300

    Primeira classe criada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console {
+    class Program {
+        static void Main(String[] args) {
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição" + P);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
 
---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace tabuleiro
+{
+	public class Posicao
+	{
+		int Linha { get; set; }
+		int Coluna { get; set; }
+
+		public Posicao(int linha, int coluna)
+		{
+
+			Linha = linha;
+            Coluna = coluna;
+
+		}
+
+        public override string ToString()
+        {
+            return Linha
+                + ", "
+                + Coluna;
+        }
+
+    }
+}
+
